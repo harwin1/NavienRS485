5dfdf037e17a5f6273444effe704afe8ec86aa0d
harwin1 <87955512+harwin1@users.noreply.github.com>
2022-12-05 22:19:35 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-SHARE_DIR=/share/bestin
+SHARE_DIR=/share/navien
 
-if [ ! -f $SHARE_DIR/bestin.js ]; then
+if [ ! -f $SHARE_DIR/Navien.js ]; then
 	mkdir $SHARE_DIR
-	mv /bestin.js $SHARE_DIR
+	mv /Navien.js $SHARE_DIR
 fi
 
 echo "[Info] Run Wallpad Controller"
 cd $SHARE_DIR
-node $SHARE_DIR/bestin.js
+node $SHARE_DIR/Navien.js
 
 # for dev
 #while true; do echo "still live"; sleep 100; done
